62b5cf7139a4cd5cf19a93d562056debba3ec38c
vboxuser <vboxuser@luizG.myguest.virtualbox.org>
2025-06-03 15:22:01 -0300

    Adiciona imagens e nova estrutura de dashboard com novos componentes e página

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,7 @@
 "use server";
 
+import { DashboardPage } from "./dashboard-page";
+
 export default async function Dashboard() {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-            <p className="text-gray-600">Bem-vindo ao seu dashboard!</p>
-        </div>
-    );
+  return <DashboardPage />;
 }
