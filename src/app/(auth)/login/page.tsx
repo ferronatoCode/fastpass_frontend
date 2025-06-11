6c4545990f2902ad240121d2cba53efdca1b8391
Lugui14 <zanelallopes9977@gmail.com>
2025-06-11 14:54:06 -0300

    feat: login page design

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,13 +2,22 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { LoginForm } from "./login-form";
 import { Toaster } from "sonner";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBolt } from "@fortawesome/free-solid-svg-icons";
 
 export default function Page() {
     return (
         <>
-            <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+            <div className="flex min-h-svh w-full mt-8 md:mt-16 justify-center p-6 md:p-10">
                 <div className="w-full max-w-sm">
                     <div className={"flex flex-col gap-6"}>
+                        <div className="mt-16 mb-12 flex flex-col items-center">
+                            <div className="flex items-center mb-4">
+                                <FontAwesomeIcon icon={faBolt} className="text-blue-600 text-3xl" />
+                                <span className="ml-2 font-bold text-3xl">FastPass</span>
+                            </div>
+                            <p className="text-gray-500 text-center">Seu acesso rápido para tickets</p>
+                        </div>
                         <Card>
                             <CardHeader>
                                 <CardTitle className="text-2xl">Login</CardTitle>
